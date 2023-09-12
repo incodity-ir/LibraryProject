97cd7b5dce181f09c4c93a5d5dee5f3a98c878ec
Ali Majidi <seyedalimajidi76@gmail.com>
2023-09-12 21:04:12 +0330

    Change & Fixed Name

--- LibraryProject.Data/BaseEntity.cs
+++ LibraryProject.Data/BaseEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryProject.Data
+namespace LibraryProject.Domain
 {
     public class BaseEntity:Entity,IDateEntity
     {
--- LibraryProject.Data/Entities/Subject.cs
+++ LibraryProject.Data/Entities/Subject.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LibraryProject.Data.Entities
+namespace LibraryProject.Domain.Entities
 {
     public  class Subject:BaseEntity
     {
