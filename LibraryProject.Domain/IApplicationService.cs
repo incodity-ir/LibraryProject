22cddc054f529d89826a9fea3cfcd5df260edf1a
Ali Majidi <seyedalimajidi76@gmail.com>
2023-09-12 23:36:34 +0330

    config ORM

---
+++ LibraryProject.Domain/IApplicationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryProject.Data
+{
+    public interface IApplicationService
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+        int SaveChanges();
+
+    }
+}
---
+++ LibraryProject.Domain/SqlServerApplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryProject.Data
+{
+    public class SqlServerApplication:DbContext,IApplicationService
+    {
+        public SqlServerApplication(DbContextOptions options):base(options)
+        {
+            
+        }
+        
+    }
+}
