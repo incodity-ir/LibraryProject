002173e53266a97a684e7412e612f23f517c1e4d
Ali Majidi <seyedalimajidi76@gmail.com>
2023-09-12 20:44:59 +0330

    Implement Basic Class

---
+++ LibraryProject.Data/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Data
+{
+    public class BaseEntity:Entity,IDateEntity
+    {
+        public virtual int ID { get; set; }
+        public virtual DateTime CreateON { get; set; }
+        public virtual DateTime UpdateON { get; set; }
+    }
+}
---
+++ LibraryProject.Data/IDateEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Data
+{
+    public interface IDateEntity
+    {
+        public DateTime CreateON { get; set; }
+        public DateTime UpdateON { get; set; }
+    }
+}
